df1277830bb85e75b61e266ecc4ac0d55a270372
Ricardo Pierini <bpierini@me.com>
2022-10-06 12:15:50 -0300

    firstCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
+  constructor(private http: HttpClient) { }
+
+
+  fatosDegatinhos:any;
+  ngOnInit(){
+    this.http.get('https://catfact.ninja/fact').subscribe(
+      data=>{
+        this.fatosDegatinhos = data;
+      },
+      error=>{
+
+      }
+    );
+  }
+
   title = 'algular-start-point';
+  public nome:string = 'People';
+
+  public pessoa = {
+    nome: "People",
+    idade: "VELHO",
+    cidade: "Sao Paulo",
+  }
+
+  getNomePessoa() {
+    return 'Nome: ' + this.pessoa.nome;
+  }
+
+
+  public lista = [
+    { titulo: 'Titulo1' },
+    { titulo: 'Titulo2' },
+    { titulo: 'Titulo3' }
+  ]
 }
